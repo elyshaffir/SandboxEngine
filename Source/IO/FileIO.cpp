9cdcb34fdba1f464d8465be3a394aef5a6558462
Ely Shaffir <elyshaffir@gmail.com>
2021-04-24 00:13:50 +0300

    Removed FileOpenException

--- Source/IO/FileIO.cpp
+++ Source/IO/FileIO.cpp
@@ -1,13 +1,14 @@
 #include <IO/FileIO.h>
+
 #include <fstream>
-#include <IO/FileOpenException.h>
+#include <stdexcept>
 
 std::vector<char> sandbox::FileIO::ReadFile(const std::string & path)
 {
 	std::ifstream file(path, std::ios::ate | std::ios::binary);
 	if (!file.is_open())
 	{
-		throw FileOpenException(path);
+		throw std::runtime_error("Could not open file in " + path);
 	}
 	size_t fileSize = static_cast<size_t>(file.tellg());
 	std::vector<char> fileBuffer(fileSize);
--- Source/IO/FileOpenException.cpp
+++
@@ -1,6 +0,0 @@
-#include <IO/FileOpenException.h>
-
-sandbox::FileOpenException::FileOpenException(const std::string & badFilePath) :
-		runtime_error("Couldn't open file at " + badFilePath)
-{
-}
